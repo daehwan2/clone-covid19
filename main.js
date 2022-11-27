47c86d2d5004455f8dd99ee72c6b81a833f405e0
daehwan2 <dhdh0482@naver.com>
2022-11-28 00:08:49 +0900

    4강 완료!

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 (()=>{
+    const actions={
+        birdFlies(key){
+            if(key){
+                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(${window.innerWidth}px)`;
+
+            }else{
+                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(-100%)`;
+            }
+        },
+
+        birdFlies2(key){
+            if(key){
+                document.querySelector('[data-index="5"] .bird').style.transform = `translate(${window.innerWidth}px, ${-window.innerHeight * 0.7}px)`;
+
+            }else{
+                document.querySelector('[data-index="5"] .bird').style.transform = `translateX(-100%)`;
+            }
+        }
+    }
+
     const stepElems = document.querySelectorAll('.step');
     const graphicElems = document.querySelectorAll('.graphic-item');
     let currentItem = graphicElems[0];
     let ioIndex;
     
     const io = new IntersectionObserver((entries, observer)=>{
+        console.log(entries);
         ioIndex = +entries[0].target.dataset.index;
     })
 
@@ -14,36 +35,46 @@
         graphicElems[i].dataset.index=i;
     }
 
-    function activate(){
+    function activate(action){
         currentItem.classList.add('visible');
+
+        if(action){
+            actions[action](true);
+        }
     }
 
-    function inactivate(){
+    function inactivate(action){
         currentItem.classList.remove('visible');
+
+        if(action){
+            actions[action](false);
+        }
     }
 
     window.addEventListener('scroll',()=>{
         let step;
         let boundingRect;
-        let temp=0;
 
         for(let i =ioIndex-1; i<ioIndex+2; i++){
             step = stepElems[i];
             if(!step) continue;
             boundingRect = step.getBoundingClientRect();
 
-            temp++;
 
             if(boundingRect.top > window.innerHeight * 0.1 && boundingRect.top < window.innerHeight *  0.8){
-              inactivate();
+              inactivate(currentItem.dataset.action);
               currentItem = graphicElems[step.dataset.index];
-              activate();
+              activate(currentItem.dataset.action);
               
             }
         }
 
        
     });
+
+    window.addEventListener('load',()=>{
+        setTimeout(()=>scrollTo(0,0), 100);
+    })
 
     activate();
 })();
